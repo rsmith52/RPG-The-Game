5987181151e703e2d470ba8ae8a81de10386408f
ray <rsmith52@wisc.edu>
2018-11-17 18:51:46 -0600

    Set up stats, test overworld scene, figured out tiles, got placeholder player sprites

---
+++ Assets/Code/Stats/CharacterStats.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour {
+
+    public Stat strength;
+    public Stat dexterity;
+    public Stat constitution;
+    public Stat willpower;
+    public Stat intelligence;
+    public Stat charisma;
+
+
+}
---
+++ Assets/Code/Stats/Stat.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Stat {
+
+    [SerializeField]
+    private int baseValue;
+
+    private List<int> modifiers = new List<int>();
+
+    public int getValue () {
+        return baseValue;
+    }
+
+    public void changeValue (int value) {
+        if (value != 0) {
+            baseValue += value;
+        }
+    }
+
+    public void addModifier (int modifier) {
+        if (modifier != 0) {
+            modifiers.Add(modifier);
+        }
+    }
+
+    public void removeModifier (int modifier) {
+        if (modifier != 0) {
+            modifiers.Remove(modifier);
+        }
+    }
+
+}
