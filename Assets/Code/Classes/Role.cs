80cbc6796804516e64a1c9ccaf781ad968cf537a
ray <rsmith52@wisc.edu>
2018-11-20 00:00:48 -0600

    Started into scriptable objects for defining items, classes, races, etc

---
+++ Assets/Code/Classes/Role.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Class", menuName = "Data/Class", order = 1)]
+public class Role : ScriptableObject {
+
+    public string roleName;
+    public string roleDescription;
+}
