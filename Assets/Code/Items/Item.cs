120bc6749a3b6b41637edef80de23d5cce8b6ff8
ray <rsmith52@wisc.edu>
2018-11-20 19:03:40 -0600

    Added base loader and game manager classes

--- Assets/Code/Items/Item.cs
+++ Assets/Code/Items/Item.cs
@@ -8,5 +8,6 @@ public class Item : ScriptableObject {
     public string itemName;
     public string itemDescription;
     public Sprite itemIcon;
+    public int itemWeight;
 	
 }
---
+++ Assets/Code/Utilities/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance = null;
+
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        }
+        else if (instance != this) {
+            Destroy(gameObject);
+        }
+        // Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+
+
+        InitGame();
+    }
+
+    // Initializes the game
+    void InitGame() {
+
+
+    }
+}
